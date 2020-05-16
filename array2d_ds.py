5cfbec290f9847509fabf11971574d0345c67a29
SATYANARAYANA UPPADA <47477333+usatya99@users.noreply.github.com>
2020-05-16 09:43:28 +0530

    Create array2d_ds.py

---
+++ array2d_ds.py
@@ -0,0 +1,54 @@
+#Arrays
+#2D ARRYAS-DS
+#!/bin/python3
+
+import math
+
+import os
+
+import random
+
+import re
+
+import sys
+
+# Complete the hourglassSum function below.
+
+def hourglassSum(arr):
+
+    res=[]
+
+    for i in range(4):
+
+        for j in range(4):
+
+            l=[]
+
+            l.extend([arr[i][j],arr[i][j+1],arr[i][j+2]])
+
+            l.append(arr[i+1][j+1])
+
+            l.extend([arr[i+2][j],arr[i+2][j+1],arr[i+2][j+2]])
+
+            res.append(sum(l))
+
+    print(res)
+
+    return max(res)
+
+if __name__ == '__main__':
+
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr = []
+
+    for _ in range(6):
+
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = hourglassSum(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
