48fc2ed1e4042ef2eddc1936a14de888b03c5631
SATYANARAYANA UPPADA <47477333+usatya99@users.noreply.github.com>
2020-05-16 09:28:50 +0530

    Create sock_merchant.py

---
+++ sock_merchant.py
@@ -0,0 +1,33 @@
+#WARM-UP CHALLENGES
+#SOCK MERCHANT
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the sockMerchant function below.
+def sockMerchant(n, ar):
+    l=[]
+    ar1=list(set(ar))
+    print(ar1)
+    for i in ar1:
+        print(ar.count(i))
+        if ar.count(i)>=2:
+            m=ar.count(i)//2
+            l.append(m)
+    return(sum(l))
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
