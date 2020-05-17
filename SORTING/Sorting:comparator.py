c1c928340c14c6088647893ebd5f7d9dd57d2839
SATYANARAYANA UPPADA <47477333+usatya99@users.noreply.github.com>
2020-05-17 08:00:17 +0530

    Create Sorting:comparator.py

---
+++ SORTING/Sorting:comparator.py
@@ -0,0 +1,22 @@
+#SORTING
+#SORTING:COMPARATOR
+
+
+from functools import cmp_to_key
+class Player:
+    def __init__(self, name, score):
+        self.name=name
+        self.score=score
+        
+    def __repr__(self):
+        pass
+        
+    def comparator(a, b):
+        if a.score>b.score:
+            return -1
+        elif a.score<b.score:
+            return 1
+        elif a.name>b.name:
+            return 1
+        else:
+            return -1
