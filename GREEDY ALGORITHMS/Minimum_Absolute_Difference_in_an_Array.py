b73571015b79a5c087f1e89130d288f6589c456c
SATYANARAYANA UPPADA <47477333+usatya99@users.noreply.github.com>
2020-05-23 09:02:31 +0530

    Create Minimum_Absolute_Difference_in_an_Array.py

---
+++ GREEDY ALGORITHMS/Minimum_Absolute_Difference_in_an_Array.py
@@ -0,0 +1,26 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the minimumAbsoluteDifference function below.
+def minimumAbsoluteDifference(arr):
+    result=[]
+    arr.sort()
+    return min([abs(x-y) for x,y in zip(arr,arr[1:])])
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = minimumAbsoluteDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
